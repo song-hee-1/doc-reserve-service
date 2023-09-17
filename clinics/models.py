cdc3ed1a6b32a074c69e9d8147deda1a36f027eb
hee <98_han@naver.com>
2023-09-17 11:47:20 +0900

    [feat/clinics] clinics 모델링

--- clinics/models.py
+++ clinics/models.py
@@ -1,3 +1,108 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampModel
+
+
+class Doctor(TimeStampModel):
+    """
+    의사 정보를 저장
+    """
+    name = models.CharField(max_length=20, help_text='의사 이름')
+    introduction = models.TextField(blank=True, help_text='의사 소개')
+    credentials = models.TextField(blank=True, help_text='의사 약력')
+    speciality = models.CharField(help_text='진료과')
+    clinic = models.ForeignKey('Clinic', related_name='doctors', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class DoctorSchedule(models.Model):
+    """
+     의사의 진료 스케줄 정보를 저장
+    """
+
+    MONDAY = 'Mon'
+    TUESDAY = 'Tue'
+    WEDNESDAY = 'Wed'
+    THURSDAY = 'Thu'
+    FRIDAY = 'Fri'
+    SATURDAY = 'Sat'
+    SUNDAY = 'Sun'
+
+    DAY_CHOICES = [
+        (MONDAY, 'Monday'),
+        (TUESDAY, 'Tuesday'),
+        (WEDNESDAY, 'Wednesday'),
+        (THURSDAY, 'Thursday'),
+        (FRIDAY, 'Friday'),
+        (SATURDAY, 'Saturday'),
+        (SUNDAY, 'Sunday'),
+    ]
+
+    REGULAR_DAY_OFF = 'Regular'
+    PERSONAL_DAY_OFF = 'Personal'
+    OTHER_DAY_OFF = 'Other'
+
+    DAY_OFF_CHOICES = [
+        (REGULAR_DAY_OFF, '정기 휴무'),
+        (PERSONAL_DAY_OFF, '개인 휴무'),
+        (OTHER_DAY_OFF, '기타 이유'),
+    ]
+
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='schedules')
+    day = models.CharField(max_length=3, choices=DAY_CHOICES, help_text='요일')
+    start_time = models.TimeField(help_text='진료 시작 시간')
+    end_time = models.TimeField(help_text='진료 종료 시간')
+    lunch_start_time = models.TimeField(blank=True, null=True, help_text='점심 시작 시간')
+    lunch_end_time = models.TimeField(blank=True, null=True, help_text='점심 종료 시간')
+    is_day_off = models.BooleanField(default=False, help_text='휴무 여부')
+    reason_for_day_off = models.CharField(max_length=10, choices=DAY_OFF_CHOICES, blank=True, help_text='휴무 이유')
+
+    def __str__(self):
+        return f"{self.doctor.name}의 스케줄 - " \
+               f"{self.get_day_display()} {self.start_time.strftime('%H:%M')} - {self.end_time.strftime('%H:%M')}"
+
+
+class Clinic(TimeStampModel):
+    """
+    병원 정보 저장
+    """
+    name = models.CharField(help_text='병원 이름')
+    non_insured_medical_category = models.ManyToManyField(
+        'NonInsuredMedicalCategory', related_name='clinics', blank=True, help_text='비급여 진료과목')
+
+    def __str__(self):
+        return self.name
+
+
+class NonInsuredMedicalCategory(TimeStampModel):
+    """
+    비급여 진료 과목 저장
+    """
+    code = models.CharField(max_length=100, help_text='비급여 진료 과목 코드')
+    name = models.CharField(max_length=40, help_text='비급여 진료 과목 이름')
+
+    def __str__(self):
+        return self.name
+
+
+class ClinicAppointment(TimeStampModel):
+    """
+    진료 요청 정보를 저장
+    """
+    PENDING_APPROVE = 'pending_approve'
+    COMPLETE = 'complete'
+
+    STATUS_CHOICES = [
+        (PENDING_APPROVE, '대기'),
+        (COMPLETE, '수락')
+    ]
+
+    doctor = models.ForeignKey('Doctor', related_name='clinic_appointments', on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=PENDING_APPROVE, help_text='진료 요청 상태')
+    desired_date = models.DateTimeField(help_text='진료 희망 날짜')
+    expired_at = models.DateTimeField(help_text='진료 요청 만료 시간')
+
+    def __str__(self):
+        return f"{self.doctor.name}의 진료 요청 - {self.desired_date.strftime('%Y-%m-%d %H:%M:%S')}"
